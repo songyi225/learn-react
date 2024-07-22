7338bce03ffab5a5eeffc1dfc04fa90372168415
songyi225 <sy920225@naver.com>
2024-07-22 16:13:52 +0900

    [학습] 새로운 연산자
    - Null 병합 연산자 (??)
    - 옵셔널 체이닝 (?.)

--- 03-ecmascript-for-react/src/01-ecmascript/03-nullish-optional-chaining.js
+++ 03-ecmascript-for-react/src/01-ecmascript/03-nullish-optional-chaining.js
@@ -5,20 +5,27 @@
 // --------------------------------------------------------------------------
 
 function nullish() {
-  let value = 0;
+  let value = 0; // 개발자의 의도!!
 
   let result = value || 100;
   console.log({ '||': result });
 
+  // nullish : Web Native
   function isNullOrUndefined(value) {
     return value === null || value === undefined ? true : false;
   }
 
-  result = !isNullOrUndefined(value) ? value : 100;
+  function isNotNullOrUndefined(value) {
+    return !isNullOrUndefined(value);
+  }
+  result = isNotNullOrUndefined(value) ? value : 100;
   console.log({ isNullOrUndefined: result });
 
   // 🔶 null 병합 연산자 코드를 작성합니다.
   // 참고: https://mzl.la/3vQUYin | https://mzl.la/3PXiOQ9
+
+  result = value ?? 100;
+  console.log({'??': result});
 }
 
 function optionalChaining() {
@@ -50,8 +57,8 @@ function optionalChaining() {
   // 🔶 optional chaining 코드를 사용해 조건 처리하세요.
   // 참고: https://mzl.la/3xx6Arc
 
-  console.log(topic.getTitle());
-  console.log(topic.getName());
+  console.log(topic?.getTitle?.());
+  console.log(topic?.getName?.());
 }
 
 function run() {
