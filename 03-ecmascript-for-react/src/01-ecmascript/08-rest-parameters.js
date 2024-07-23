4eae0d8e196817b43edb3fccb8d80063555a6cb3
songyi225 <sy920225@naver.com>
2024-07-23 09:34:39 +0900

    [학습] 나머지 매개변수

--- 03-ecmascript-for-react/src/01-ecmascript/08-rest-parameters.js
+++ 03-ecmascript-for-react/src/01-ecmascript/08-rest-parameters.js
@@ -2,15 +2,28 @@
 // rest parameters
 
 function _sum() {
-  const numbers = Array.from(arguments);
-  return numbers.reduce((result, number) => result + number, 0);
+  console.log(Array.isArray(arguments), arguments.length); // 배열로 오인할 수 있지만 배열은 아니다
+
+  // Array.prototype.slice.call(arguments) 요걸 더 쉽게 이해할 수 있도록 Array.from으로 확인할 수 있다!
+  const numbers = Array.from(arguments); // array? like array object? 유사배열임!
+
+  console.log(Array.isArray(numbers), numbers.length);
+
+  return numbers.reduce(
+    /* reducer function */ (result, number) => result + number,
+    0
+  );
 }
 
 // 🔶 나머지 매개변수를 사용해 sum 함수 코드 로직을 다시 작성합니다.
 // 참고: https://mzl.la/43Ro9yp
-const sum = () => {};
+const sum = (...numbers) => numbers.reduce((total, number) => total + number);
+// console.log(Array.isArray(numbers));
+// console.log(arguments);
+// return undefined
+// console.log(numbers);
 
-let result1_1 = _sum(2, 3, 9, 12, 105);
+let result1_1 = _sum(2, 3, 9, 12, 105); // arguments가 전달
 let result1_2 = sum(2, 3, 9, 12, 105);
 console.log(Object.is(result1_1, result1_2));
 
