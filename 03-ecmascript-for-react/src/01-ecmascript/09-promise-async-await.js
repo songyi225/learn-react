ce0cfb27b2b05a2d5e08af49c875ff6878e7d5f0
songyi225 <sy920225@naver.com>
2024-07-23 14:40:01 +0900

    [학습] promise.all , race

--- 03-ecmascript-for-react/src/01-ecmascript/09-promise-async-await.js
+++ 03-ecmascript-for-react/src/01-ecmascript/09-promise-async-await.js
@@ -100,12 +100,12 @@ async function practice3() {
   console.log("end!!");
 }
 
-practice3();
+// practice3();
 
 // --------------------------------------------------------------------------
 // Promise.all, Promise.race
 
-const MAX_TIMEOUT = 1000;
+const MAX_TIMEOUT = 2000;
 
 const promise1 = () =>
   new Promise((resolve) => {
@@ -128,9 +128,26 @@ const promise2 = () =>
 const promise3 = () => Promise.reject("❌ 오류 발생!");
 
 // Promise.all
+// [promise1, promise2, promise3, ... , promiseN].then(()=>{...})
 // 참고: https://mzl.la/49EvJxn
 // 🔶 Promise.all 메서드를 사용해 모든 Promise가 실행된 이후 콜백되도록 실습을 진행합니다.
+
+Promise.all([promise1(), promise2()])
+  .then((result) => {
+    console.log(result); // [ result1, result2 ]
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 // Promise.race
 // 참고: https://mzl.la/49EvJxn
 // 🔶 Promise.race 메서드를 사용해 응답이 가장 빠른 Promise 값만 출력되도록 실습을 진행합니다.
+
+Promise.race([promise1(), promise2()])
+  .then((winner) => {
+    console.log(winner); // winner
+  })
+  .catch((error) => {
+    console.error(error);
+  });
